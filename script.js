c0f422830c1f02cc532ab6bae11abae6cb7f8eec
levitate-dev <112495962+Naveen-3354@users.noreply.github.com>
2025-07-25 13:17:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-
-
-class Car{
-    constructor(name){
-        this.name = "bmw"
-    }
-
-    static forward(){
-        console.log("move 10m");
-    }
-}
-
-const car = new Car()
-console.log(car.name);
-
-Car.forward(s)
